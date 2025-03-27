ee07c0fd269a4bff7da012930c743ac9bc834bdf
TOMAYOSHI <108039040+TOMAYOSHI@users.noreply.github.com>
2025-03-27 10:54:39 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-northeast-1_6rW91pLqc', // e.g. us-east-2_uXboG5pAb
-        //userPoolClientId: '5qcv2smtkd6mo5mj6p8k2tr1ka', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        userPoolClientId: '70tj2mgk1udkf7dadnr1m4vmjo', 
+        userPoolClientId: '5qcv2smtkd6mo5mj6p8k2tr1ka', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        //userPoolClientId: '70tj2mgk1udkf7dadnr1m4vmjo', 
         region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
